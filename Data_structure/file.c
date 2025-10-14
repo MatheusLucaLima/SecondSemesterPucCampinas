d76d2cef0ea027cd26f48f80dcb087d99f632406
MATHEUS ANTONY LUCAS LIMA <matheus.all@puccampinas.edu.br>
2025-10-14 20:16:47 -0300

    Fix 'patient' mispeling, add some stuff

--- Data_structure/file.c
+++ Data_structure/file.c
@@ -2,38 +2,58 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct Pacient{
+typedef struct Patient{
     char name[100];
     int age;
     int id;
-    struct Pacient *next;
-}pacient;
+    struct Patient *next;
+}patient;
 
-typedef struct pacient_pointer{
-    pacient *first;
-    pacient *last;
-}pointers;
 
-pacient *add_pacient(pacient *first);
+void add_patient(pacient *first);
+void treat_patient
 
 int main(){
-    pacient *first = NULL;
-    pointers *help = NULL;
+    patient *first = NULL;
+    add_patient(first);
 
 }
 
-pacient *add_pacient(pacient *first){
-    pacient *new_pacient = (pacient *) malloc (sizeof(pacient));
-    if (new_pacient == NULL){
+void add_patient(patient *first){
+    patient *new_patient = (patient *) malloc (sizeof(patient));
+    if (new_patient == NULL){
         printf("Error on memory allocation.\n");
         exit(-1);
     }
-    printf("Pacient's name:\n");
+    printf("Patient's name:\n");
     char name[100];
     scanf(" %[^\n]", name);
-    printf("Pacient's age:\n");
+    printf("Patient's age:\n");
     int age;
     scanf(" %d", &age);
-    printf("Pacient's id:\n");
+    printf("Patient's id:\n");
+    int id;
+    scanf(" %d", &id);
+
+    strcpy(new_patient->name, name);
+    new_patient -> age = age;
+    new_patient -> id = id;
+    new_patient -> next = NULL;
+
+    if (first == NULL){
+        first = new_patient;
+        return;
+    }
+
+    patient *aux = first;
+    
+    while(aux){
+        if(aux->next == NULL){
+            aux -> next = new_patient;
+            break;
+        }
+        aux = aux -> next;
+    }
+    
 
 }
